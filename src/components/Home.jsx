ff379f161b42d5c447a332fe633314f68ba42064
Yomna Salah <syomna444@gmail.com>
2026-05-24 01:30:32 +0300

    Updated CV

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import { motion } from "framer-motion";
-import cv from "../assets/resume/Yomna_Salah_Attallah_Flutter_Developer_CV.pdf";
+import cv from "../assets/resume/Yomna_Salah_Flutter_Developer_CV.pdf";
 
 import { INK_MID, RULE } from "../utils/tokens";
 import { fadeUp } from "../utils/animations";
@@ -31,7 +31,7 @@ const scrollToContact = () => {
 
 const downloadCV = () => {
   const link = document.createElement("a");
-  link.download = "Yomna_Salah_Attallah_Flutter_Developer_CV.pdf";
+  link.download = "Yomna_Salah_Flutter_Developer_CV.pdf";
   link.href = cv;
   link.click();
 };
